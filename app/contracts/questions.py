51b785457971bdec1d9aface83c134ab17aed6e6
OST <cpython.projects@gmail.com>
2026-07-03 12:32:41 +0400

    community_pulse 03/07

--- app/contracts/questions.py
+++ app/contracts/questions.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel, ConfigDict
 from app.models import Statistics
 from app.schemas import *
 
@@ -17,5 +18,6 @@ class QuestionDetailResponse(QuestionRead):
     statistics: StatisticsRead | None = None
 
 
-class QuestionListResponse(QuestionRead):
-    pass
+class QuestionListResponse(BaseModel):
+    items: list[QuestionResponse]
+    count: int
